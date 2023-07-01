e3a7f25115951ddb683e49edda432c1b9ab8f797
AvasBrahma <avasbrahma@gmail.com>
2023-07-01 19:55:47 +0530

    routes setup and mongodb configuration

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const router=express.Router();
 
 const homeController=require('../controllers/home_controller');
 
+router.use('/doctors', require('./doctors'));
+
+router.use('/patients', require('./patients'));
+
+
 router.get('/', homeController.home);
 
 
